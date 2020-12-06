b23d988a3edd951b131b1cd89347395864e22e98
Pedro Klepa <pedro.klepa@gmail.com>
2020-12-06 20:42:22 +0000

    Boilerplate cleanup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './assets/fonts/fonts.css'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>In construction.</h1>
+    </div>
+  );
+}
+
+export default App;
