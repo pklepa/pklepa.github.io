292aa7a5ed0bb551eb023e1cf30290c896e7a8a2
Pedro Klepa <pedro.klepa@gmail.com>
2020-12-07 11:27:40 +0000

    Add styled-components, husky, prettier, lint, as well as basic styling for now

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import React from 'react'
-import './assets/fonts/fonts.css'
-import './App.css'
+import React from "react";
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
+import { Reset } from "styled-reset";
+
+const theme = {
+  font: "Dosis",
+  primary: "#333",
+  secondary: "#999",
+};
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    color: ${(props) => props.theme.secondary};
+    font-family: 'Dosis', sans-serif;
+    font-weight: 400;
+    font-size: 18px;  
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  min-height: 100vh;
+  background-color: ${(props) => props.theme.primary};
+`;
 
 function App() {
   return (
-    <div>
-      <h1>In construction.</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Reset />
+      <GlobalStyle />
+      <Container>
+        <h1>In construction.</h1>
+      </Container>
+    </ThemeProvider>
   );
 }
 
