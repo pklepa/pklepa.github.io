1f220ca8112010612dcb0901cc95f3f1fc1d5e2e
Pedro Klepa <pedro.klepa@gmail.com>
2020-12-07 18:43:04 +0000

    Insert icons and links for social accounts

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
 import { Reset } from "styled-reset";
 
+import { Github, LinkedinSquare } from "styled-icons/boxicons-logos";
+import { Mail } from "styled-icons/ionicons-solid";
+import { Construction } from "styled-icons/material-rounded";
+
 const theme = {
   font: "Dosis",
   primary: "#333",
@@ -27,6 +31,38 @@ const Container = styled.div`
   width: 100%;
   min-height: 100vh;
   background-color: ${(props) => props.theme.primary};
+
+  span {
+    text-align: center;
+    line-height: 1.5em;
+    margin-bottom: 20px;
+  }
+`;
+
+const GithubIcon = styled(Github)``;
+const LinkedinIcon = styled(LinkedinSquare)``;
+const MailIcon = styled(Mail)``;
+
+const IconsContainer = styled.div`
+  display: flex;
+
+  a {
+    margin-right: 12px;
+
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+
+  svg {
+    width: 45px;
+    color: ${(props) => props.theme.secondary};
+    transition: 400ms;
+
+    &:hover {
+      color: ${(props) => props.theme.highlight};
+    }
+  }
 `;
 
 function App() {
@@ -35,7 +71,22 @@ function App() {
       <Reset />
       <GlobalStyle />
       <Container>
-        <h1>In construction.</h1>
+        <span>
+          Oops, it seems like you got here a little early. <br />
+          You can still reach me through one of these:
+        </span>
+
+        <IconsContainer>
+          <a target="_blank" href="https://github.com/pklepa">
+            <GithubIcon />
+          </a>
+          <a target="_blank" href="https://www.linkedin.com/in/pedro-klepa/">
+            <LinkedinIcon />
+          </a>
+          <a target="_blank" href="mailto:name@email.com">
+            <MailIcon />
+          </a>
+        </IconsContainer>
       </Container>
     </ThemeProvider>
   );
