4eaa8e5b0057f83088708e7f3157e027e60cb78d
Pedro Klepa <pedro.klepa@gmail.com>
2021-02-19 17:58:55 +0000

    Update projects_data's tools with to new array format

--- src/assets/data/projects_data.js
+++ src/assets/data/projects_data.js
@@ -36,48 +36,36 @@ export const projects_data = [
     githubUrl: "https://github.com/pklepa/pencil-blog",
     liveDemoUrl: "https://blog-pencil.herokuapp.com/",
   },
-  // {
-  //   photoUrl:
-  //     "https://user-images.githubusercontent.com/22618438/106356508-f57d3600-62f7-11eb-9677-8dcf6ef0e49d.png",
-  //   title: "Shortly",
-  //   tools: {
-  //     react: true,
-  //     styledComponents: true,
-  //   },
-  //   description:
-  //     "Shortly is a landing page for a link customization service that integrates the shrtco.de API for URL shortening. Made as solution for a Frontendmentor.io challenge. ",
-  //   githubUrl: "https://github.com/pklepa/url-shortening",
-  //   liveDemoUrl: "https://pklepa.github.io/url-shortening/",
-  // },
-  // {
-  //   photoUrl:
-  //     "https://user-images.githubusercontent.com/22618438/101240748-20c51980-36e9-11eb-9190-4288896bb716.png",
-  //   title: "Discount Discord",
-  //   tools: {
-  //     react: true,
-  //     firebase: true,
-  //     styledComponents: true,
-  //     framer: true,
-  //   },
-  //   description:
-  //     "A clone of Discord's user interface and some of its features such as real-time chat, user authentication (Google account) and distinct channel chat rooms.",
-  //   githubUrl: "https://github.com/pklepa/discord-clone",
-  //   liveDemoUrl: "https://pklepa.github.io/discord-clone/",
-  // },
-  // {
-  //   photoUrl:
-  //     "https://user-images.githubusercontent.com/22618438/101240380-e8244080-36e6-11eb-87b9-3ca04851c74e.png",
-  //   title: "Planetarium",
-  //   tools: {
-  //     react: true,
-  //     styledComponents: true,
-  //     framer: true,
-  //   },
-  //   description:
-  //     "Clean design for an e-commerce application with shopping cart capabilities. Main focus was practicing and deploying a React Router application.",
-  //   githubUrl: "https://github.com/pklepa/shopping-cart",
-  //   liveDemoUrl: "https://pklepa.github.io/shopping-cart/",
-  // },
+  {
+    photoUrl:
+      "https://user-images.githubusercontent.com/22618438/106356508-f57d3600-62f7-11eb-9677-8dcf6ef0e49d.png",
+    title: "Shortly",
+    tools: ["react", "styled-components"],
+    description:
+      "Shortly is a landing page for a link customization service that integrates the shrtco.de API for URL shortening. Made as solution for a Frontendmentor.io challenge. ",
+    githubUrl: "https://github.com/pklepa/url-shortening",
+    liveDemoUrl: "https://pklepa.github.io/url-shortening/",
+  },
+  {
+    photoUrl:
+      "https://user-images.githubusercontent.com/22618438/101240748-20c51980-36e9-11eb-9190-4288896bb716.png",
+    title: "Discount Discord",
+    tools: ["react", "firebase", "styled-components", "framer-motion"],
+    description:
+      "A clone of Discord's user interface and some of its features such as real-time chat, user authentication (Google account) and distinct channel chat rooms.",
+    githubUrl: "https://github.com/pklepa/discord-clone",
+    liveDemoUrl: "https://pklepa.github.io/discord-clone/",
+  },
+  {
+    photoUrl:
+      "https://user-images.githubusercontent.com/22618438/101240380-e8244080-36e6-11eb-87b9-3ca04851c74e.png",
+    title: "Planetarium",
+    tools: ["react", "styled-components", "framer-motion"],
+    description:
+      "Clean design for an e-commerce application with shopping cart capabilities. Main focus was practicing and deploying a React Router application.",
+    githubUrl: "https://github.com/pklepa/shopping-cart",
+    liveDemoUrl: "https://pklepa.github.io/shopping-cart/",
+  },
   // {
   //   photoUrl:
   //     "https://user-images.githubusercontent.com/22618438/103463379-d48aea80-4d23-11eb-8b48-96064477351a.png",
@@ -93,32 +81,26 @@ export const projects_data = [
   //   githubUrl: "https://github.com/pklepa/inventory-app",
   //   liveDemoUrl: "https://nameless-woodland-81012.herokuapp.com/",
   // },
-  // {
-  //   photoUrl:
-  //     "https://user-images.githubusercontent.com/22618438/92652759-c3752400-f2c3-11ea-9f4b-bb0d687b1675.png",
-  //   title: "Sci-find",
-  //   tools: {
-  //     react: true,
-  //     firebase: true,
-  //   },
-  //   description:
-  //     "A visual puzzle application, using some densely packed illustrations as in the Where's Waldo series. The app uses Firebase as a database service to save the player's highscore.",
-  //   githubUrl: "https://github.com/pklepa/where-is-waldo",
-  //   liveDemoUrl: "https://pklepa.github.io/where-is-waldo/",
-  // },
-  // {
-  //   photoUrl:
-  //     "https://user-images.githubusercontent.com/22618438/92491599-ca6b3c00-f1c8-11ea-8b7c-ace78dafa572.png",
-  //   title: "Battleship",
-  //   tools: {
-  //     react: true,
-  //     jest: true,
-  //   },
-  //   description:
-  //     "Recreation of the classic pen and paper game of Battleship on the browser. Made entirely on Test Driven Development using automated tests with Jest.",
-  //   githubUrl: "https://github.com/pklepa/battleship",
-  //   liveDemoUrl: "https://pklepa.github.io/battleship/",
-  // },
+  {
+    photoUrl:
+      "https://user-images.githubusercontent.com/22618438/92652759-c3752400-f2c3-11ea-9f4b-bb0d687b1675.png",
+    title: "Sci-find",
+    tools: ["react", "firebase"],
+    description:
+      "A visual puzzle application, using some densely packed illustrations as in the Where's Waldo series. The app uses Firebase as a database service to save the player's highscore.",
+    githubUrl: "https://github.com/pklepa/where-is-waldo",
+    liveDemoUrl: "https://pklepa.github.io/where-is-waldo/",
+  },
+  {
+    photoUrl:
+      "https://user-images.githubusercontent.com/22618438/92491599-ca6b3c00-f1c8-11ea-8b7c-ace78dafa572.png",
+    title: "Battleship",
+    tools: ["react", "jest"],
+    description:
+      "Recreation of the classic pen and paper game of Battleship on the browser. Made entirely on Test Driven Development using automated tests with Jest.",
+    githubUrl: "https://github.com/pklepa/battleship",
+    liveDemoUrl: "https://pklepa.github.io/battleship/",
+  },
   // // {
   // //   photoUrl:
   // //     "https://user-images.githubusercontent.com/22618438/91083178-39209500-e620-11ea-9d6e-ed4a6720473b.png",
@@ -174,16 +156,14 @@ export const projects_data = [
   //   liveDemoUrl: "https://pklepa.github.io/calculator/",
   // },
 
-  // {
-  //   photoUrl:
-  //     "https://user-images.githubusercontent.com/22618438/102665235-5dc6ec80-417c-11eb-81dc-3081f4656bdc.png",
-  //   title: "Pomodoro Timer",
-  //   tools: {
-  //     javascript: true,
-  //   },
-  //   description:
-  //     "Simple browser clock for using Pomodoro Technique (25 minutes of productive activity, 5 minutes of rest). The project is all in vanilla JavaScript and was made as a pair programming exercise with a fellow student.",
-  //   githubUrl: "https://github.com/pklepa/pomodoro",
-  //   liveDemoUrl: "https://pklepa.github.io/pomodoro",
-  // },
+  {
+    photoUrl:
+      "https://user-images.githubusercontent.com/22618438/102665235-5dc6ec80-417c-11eb-81dc-3081f4656bdc.png",
+    title: "Pomodoro Timer",
+    tools: ["vanilla-js"],
+    description:
+      "Simple browser clock for using Pomodoro Technique (25 minutes of productive activity, 5 minutes of rest). The project is all in vanilla JavaScript and was made as a pair programming exercise with a fellow student.",
+    githubUrl: "https://github.com/pklepa/pomodoro",
+    liveDemoUrl: "https://pklepa.github.io/pomodoro",
+  },
 ];
